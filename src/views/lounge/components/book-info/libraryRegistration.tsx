578912ecde12edb3829d058bb930c59502a505bf
Eunjin Oh <ibsuny3214@naver.com>
2025-07-20 22:19:19 +0900

    ✨ Feat: 서재에 등록 버튼-독서상태 로직 #40

--- src/views/lounge/components/book-info/libraryRegistration.tsx
+++ src/views/lounge/components/book-info/libraryRegistration.tsx
@@ -1,6 +1,7 @@
 import chevron_left from '/src/assets/button/book-info/chevron-left.svg';
 import calendar from '/src/assets/button/book-info/calendar.svg';
 import bookImgEx from '/src/assets/button/book-info/bookImgEx.png';
+import { useState } from 'react';
 
 interface LibraryRegistrationProps {
   onRegister: () => void;
@@ -11,6 +12,10 @@ const LibraryRegistration = ({
   onRegister,
   closeModal,
 }: LibraryRegistrationProps) => {
+  const [readingStatus, setReadingStatus] = useState(1);
+  const handleClickSave = () => {
+    console.log('저장 버튼 클릭됨' + readingStatus);
+  };
   return (
     <div className="fixed inset-0 bg-black/70 z-50 flex items-center justify-center">
       <div className="w-[440px] flex flex-col justify-start items-center bg-[rgba(45,40,34,1)] rounded-2xl px-17 relative">
@@ -52,7 +57,7 @@ const LibraryRegistration = ({
                 날짜
               </div>
               <div className="flex items-center justify-between w-[207px] rounded-sm bg-[rgba(31,28,25,0.5)] px-5 py-[9px] box-border">
-                <div className=" text-white text-sm not-italic font-normal  ">
+                <div className="text-white text-sm not-italic font-normal  ">
                   2025.05.30 (금)
                 </div>
                 <div className="w-7 h-7">
@@ -67,13 +72,14 @@ const LibraryRegistration = ({
             독서상태
           </div>
           <div className="flex items-center justify-between w-full gap-[11px]">
-            <button className="w-[117px] h-[38px] rounded border border-solid border-nook-br-100 px-10 py-2 text-[rgba(255,255,255,0.50)] text-center text-sm not-italic font-normal leading-[22px]">
+            <button className={`w-[117px] h-[38px] rounded border border-solid border-nook-br-100 px-10 py-2  text-center text-sm not-italic font-normal leading-[22px] ${readingStatus === 1 ? 'bg-nook-br-100 text-white' : 'text-[rgba(255,255,255,0.50)]'}`} 
+onClick={() => setReadingStatus(1)}>
               독서중
             </button>
-            <button className="w-[117px] h-[38px] rounded border border-solid border-nook-br-100 px-10 py-2 text-[rgba(255,255,255,0.50)] text-center text-sm not-italic font-normal leading-[22px]">
+            <button className={`w-[117px] h-[38px] rounded border border-solid border-nook-br-100 px-10 py-2 text-center text-sm not-italic font-normal leading-[22px] ${readingStatus === 2 ? 'bg-nook-br-100 text-white' : 'text-[rgba(255,255,255,0.50)]'}`} onClick={() => setReadingStatus(2)}>
               완독
             </button>
-            <button className="w-[117px] h-[38px] rounded border border-solid border-nook-br-100 px-10 py-2 text-[rgba(255,255,255,0.50)] text-center text-sm not-italic font-normal leading-[22px]">
+            <button className={`w-[117px] h-[38px] rounded border border-solid border-nook-br-100 px-10 py-2 text-center text-sm not-italic font-normal leading-[22px] ${readingStatus === 3 ? 'bg-nook-br-100 text-white' : 'text-[rgba(255,255,255,0.50)]'}`} onClick={() => setReadingStatus(3)}>
               찜
             </button>
           </div>
