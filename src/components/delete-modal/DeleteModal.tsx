b1cf0177064649f827e48523753c208bcb3f01c4
woojo230 <woojunghyck@naver.com>
2025-07-21 22:22:55 +0900

    ♻️ Refactor: 공용 삭제 컴포넌트 수정

--- src/components/delete-modal/DeleteModal.tsx
+++ src/components/delete-modal/DeleteModal.tsx
@@ -1,18 +1,30 @@
 interface DeleteBtnProps {
+  usage: 'library' | 'book-info';
   onDelete: () => void;
   closeModal: () => void;
 }
 
-const DeleteBtn = ({ onDelete, closeModal }: DeleteBtnProps) => {
+const DeleteBtn = ({ onDelete, closeModal, usage }: DeleteBtnProps) => {
   return (
     <div className="fixed inset-0 bg-black/70 z-50 flex items-center justify-center">
       <div className="w-[44.9rem] h-[19.8rem] flex flex-col justify-center items-center bg-[rgba(45,40,34,1)] rounded-2xl p-6 relative">
-        <p className="text-md font-normal text-nook-100 mb-2">
-          서재에서 삭제하시겠습니까?
-        </p>
-        <p className="text-md font-normal text-nook-100 mb-6">
-          작성한 도서 기록도 함께 삭제됩니다.
-        </p>
+        {usage === 'library' ? (
+          <>
+            <p className="text-md font-normal text-nook-100 mb-2">
+              서재에서 삭제하시겠습니까?
+            </p>
+            <p className="text-md font-normal text-nook-100 mb-6">
+              작성한 도서 기록도 함께 삭제됩니다.
+            </p>
+          </>
+        ) : (
+          <>
+            <p className="text-md font-normal text-nook-100 mb-2">
+              리뷰를 삭제하시겠습니까?
+            </p>
+          </>
+        )}
+
         <div className="absolute bottom-10 right-10 flex space-x-4">
           <button
             onClick={onDelete}
