397488d5e74cefe6750604f8362c2fe13c74251d
seoyeon0103 <angel6462@naver.com>
2025-07-24 18:39:44 +0900

    ♻️  Refactor:ReadingRoomCard size 조정 & 콜백 형태로 store 객체 변경하기

--- src/views/reading-room/components/AddReadingRoom.tsx
+++ src/views/reading-room/components/AddReadingRoom.tsx
@@ -2,8 +2,20 @@ import React from 'react';
 
 const AddReadingRoom = () => {
     return(
-        <div className='relative flex w-62 height-[400px] group'>
-            
+        <div 
+            className='relative flex justify-center items-center w-124 h-200 group rounded-xl border border-dashed'
+            style={{
+                backgroundColor: 'rgba(66, 60, 53, 0.10)',
+                borderColor: 'rgba(66, 60, 53, 0.40)'}}>
+            <button
+                className='flex justify-center items-center w-20 h-20 rounded-full border border-dashed'
+                style={{borderColor: 'rgba(121, 191, 201, 0.50)'}}>
+                <svg 
+                    xmlns="http://www.w3.org/2000/svg" 
+                    width="22" height="22" viewBox="0 0 22 22" fill="none">
+                    <path d="M11 0.75V11M11 21.25V11M11 11H1M11 11H21" stroke="#7ABFC9" stroke-linecap="round"/>
+                </svg>
+            </button>
         </div>
     );
 }
--- src/views/reading-room/page/index.tsx
+++ src/views/reading-room/page/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import ReadingRoomCardList from '../components/ReadingRoomCardList';
+//import ReadingRoomCardList from '../components/ReadingRoomCardList';
+import AddReadingRoom from '../components/AddReadingRoom';
 
 const ReadingRoom = () => {
   return (
     <div className='flex flex-col justify-center items-center'>
       <div className='flex items-center justify-center'>
-        <ReadingRoomCardList/>
+        {/* <ReadingRoomCardList/> */}
+        <AddReadingRoom/>
       </div>
     </div>
   );
