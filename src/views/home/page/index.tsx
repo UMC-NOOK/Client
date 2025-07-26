b7b027c421f3bc3862de0e57440f935c167e15d3
suzy_mac <suzy@suzy-macui-MacBookAir.local>
2025-07-26 18:17:16 +0900

    ✨ Feat: 홈 UI 구현

--- src/views/home/page/index.tsx
+++ src/views/home/page/index.tsx
@@ -1,4 +1,6 @@
+import BookCategoryChart from '../../../components/home/BookCategoryChart';
 import BookStatusBox from '../../../components/home/BookStatusBox';
+import BookSummaryCard from '../../../components/home/Booksummary';
 import CurrentlyReadingBox from '../../../components/home/CurrentlyReadingBox';
 import MiddleNookie from '../../../components/home/MiddleNookie';
 import RecentJournalBox from '../../../components/home/RecentJournalBox';
@@ -16,7 +18,20 @@ const Main = () => {
       </section>
       <section>
       <MiddleNookie/>
-
+      </section>
+      <section className="w-[246px] flex flex-col gap-[10px]">
+        <BookSummaryCard/>
+        <BookCategoryChart categories={[]} />
+        <BookCategoryChart
+          categories={[
+        { name: '에세이', count: 10, latestTimestamp: Date.parse('2025-07-25T10:00:00Z') },
+        { name: '자기계발', count: 8, latestTimestamp: Date.parse('2025-07-24T15:00:00Z') },
+        { name: '소설', count: 6, latestTimestamp: Date.parse('2025-07-23T09:30:00Z') },
+        { name: '시', count: 4, latestTimestamp: Date.parse('2025-07-22T08:10:00Z') },
+        { name: '과학', count: 2, latestTimestamp: Date.parse('2025-07-21T12:40:00Z') },
+        { name: '역사', count: 1, latestTimestamp: Date.parse('2025-07-20T11:20:00Z') },
+          ]}
+        />
       </section>
     </main>
   );
