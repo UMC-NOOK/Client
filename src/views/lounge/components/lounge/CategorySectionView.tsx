e1a8efaa0eaf10c8b8804a54ae5704f01d4bb962
seoyeon0103 <angel6462@naver.com>
2025-07-29 00:06:30 +0900

    ♻️  Refactor: Font-pretendard 지우기 & Lounge UI 리펙토링

--- src/views/lounge/components/lounge/CategorySectionView.tsx
+++ src/views/lounge/components/lounge/CategorySectionView.tsx
@@ -58,22 +58,22 @@ const CategorySectionView = ({
     return <RecommendView />;
   }
 
-    return (
-        <div className='flex flex-col items-start justify-center w-full mt-9'>
-            <div className='flex flex-col items-start justify-center w-full mt-1 mb-10 text-white font-pretendard'
-                style={{ borderTop: "1px solid rgba(85, 83, 81, 0.7)" }}>
-                <div className='text-base mt-10'>
-                    {selected}
-                </div>
-                <div className='text-lg font-semibold mt-1'>
-                    따끈따끈 신간 도서를 만나보세요!
-                </div>
-                <div className='flex items-center justify-center mt-10'>
-                    <BookListSection />
-                </div>
+      return (
+        <div className="flex flex-col items-start justify-center w-full mt-10">
+          <div
+            className="flex flex-col items-start justify-center w-full mt-1 mb-10 text-white"
+            style={{ borderTop: '1px solid rgba(85, 83, 81, 0.7)' }}
+          >
+            <div className="text-base mt-10">{selected}</div>
+            <div className="text-lg font-semibold mt-1">
+              따끈따끈 신간 도서를 만나보세요!
             </div>
+            <div className="flex items-center justify-center mt-10">
+              <BookListSection />
+            </div>
+          </div>
 
-      <div className="flex flex-col items-start justify-center w-full mt-15 text-white font-pretendard">
+      <div className="flex flex-col items-start justify-center w-full mt-15 text-white">
         <div className="flex items-center w-full">
           <span className="flex text-lg font-bold whitespace-nowrap mr-4">
             분야별 베스트셀러 Top 10
@@ -85,7 +85,7 @@ const CategorySectionView = ({
         </div>
         <div className="flex flex-col w-full">
           {bestCategories.map((cat, index) => (
-            <div key={index} className="text-white font-pretendard">
+            <div key={index} className="text-white">
               <div className="mt-20">
                 <span className="text-base">| {cat.title}</span>
               </div>
