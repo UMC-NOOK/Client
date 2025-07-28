259a4c0dadc147a6f69ee2d0f1f04b42e8f476d9
seoyeon0103 <angel6462@naver.com>
2025-07-29 02:15:09 +0900

    ✨ Feat: 리딩룸 가입 모달 UI

--- src/views/reading-room/page/index.tsx
+++ src/views/reading-room/page/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import JoinReadingRoomModal from '../JoinReadingRoomModal';
 
 const ReadingRoom = () => {
-  return <div></div>;
+  return (
+    <div className='flex min-h-screen w-full justify-center items-center'>
+      <JoinReadingRoomModal/>
+    </div>
+  );
 };
 
 export default ReadingRoom;
