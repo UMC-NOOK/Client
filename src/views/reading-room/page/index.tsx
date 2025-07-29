04bebdaa615628370a04dca02ed24e1d00980eb0
seoyeon0103 <angel6462@naver.com>
2025-07-29 17:53:12 +0900

    ✨ Feat: 내 리딩룸과 모든 리딩룸 보기 navigate로 연결하기

--- src/views/reading-room/page/index.tsx
+++ src/views/reading-room/page/index.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 
-//import MyReadingRoomCardList from '../components/views/MyReadingRoomCardList';
-import ReadingRoomList from '../components/views/ReadingRoomList';
+import MyReadingRoomCardList from '../components/views/MyReadingRoomCardList';
 
 const ReadingRoom = () => {
   return (
     <div className='flex flex-col justify-center items-center'>
       <div className='flex items-center justify-center'>
-        {/* <MyReadingRoomCardList/> */}
-        <ReadingRoomList/>
+        <MyReadingRoomCardList/> 
       </div>
     </div>
   );
