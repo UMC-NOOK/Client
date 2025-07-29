2a805ae3ea61873844ed23307aa5430a6d7fc142
seoyeon0103 <angel6462@naver.com>
2025-07-29 15:08:46 +0900

    ✨ Feat: 태그 더보기 모달 UI 구현

--- src/views/reading-room/page/index.tsx
+++ src/views/reading-room/page/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import MyReadingRoomCardList from '../components/MyReadingRoomCardList';
+import ShowMoreTagsModal from '../components/Modals/ShowMoreTagsModal';
 
 const ReadingRoom = () => {
   return (
     <div className='flex flex-col justify-center items-center'>
       <div className='flex items-center justify-center'>
-        <MyReadingRoomCardList/>
+        {/* <MyReadingRoomCardList/> */}
+        <ShowMoreTagsModal/>
       </div>
     </div>
   );
