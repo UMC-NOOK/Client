fac4fc71fc69f76263be257a2ab973212ac9e903
seoyeon0103 <angel6462@naver.com>
2025-07-29 09:14:03 +0900

    ✨ Feat: 태그 더보기 모달 UI 구현

--- src/views/reading-room/page/index.tsx
+++ src/views/reading-room/page/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import JoinReadingRoomModal from '../JoinReadingRoomModal';
+import ShowMoreTagsModal from '../ShowMoreTagsModal';
 
 const ReadingRoom = () => {
   return (
     <div className='flex min-h-screen w-full justify-center items-center'>
-      <JoinReadingRoomModal/>
+      <ShowMoreTagsModal/>
     </div>
   );
 };
