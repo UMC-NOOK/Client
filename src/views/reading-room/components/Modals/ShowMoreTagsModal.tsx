d0d524e5bb3c5e00a11778b8ce6e17b3e0ad7e1e
seoyeon0103 <angel6462@naver.com>
2025-07-31 01:13:05 +0900

    ✨ Feat: 리딩룸 생성하기 UI 구현

--- src/views/reading-room/components/Modals/ShowMoreTagsModal.tsx
+++ src/views/reading-room/components/Modals/ShowMoreTagsModal.tsx
@@ -1,6 +1,14 @@
-import React, { useState } from "react";
+import React, { Dispatch, SetStateAction } from "react";
 
-const ShowMoreTagsModal = () => {
+const ShowMoreTagsModal = ({
+    selected,
+    setSelected,
+    onClose,
+    } : {
+        selected:string[];
+        setSelected: Dispatch<SetStateAction<string[]>>;
+        onClose: () => void;
+    }) => {
     const goalTags = ['자유독서', '필사', '독서기록', '뽀모도로', '루틴'];
     const timeTags = ['아침', '낮', '밤', '주말', '퇴근길', '출근길'];
     const ageTags = ['10대','20대', '30대', '40대', '50대'];
@@ -10,248 +18,111 @@ const ShowMoreTagsModal = () => {
     const fieldTags3 = ['여행', '요리', '가정', '청소년','eBook'];
     const fieldTags4 = ['일본도서', '외국도서', '영어 원서'];
 
-    const [selected, setSeleted] = useState<string[]>([]);
-
     const toggleTag = (tag: string) => {
-        setSeleted((prev) =>
-            prev.includes(tag) ? prev.filter((t)=> t !== tag) : [...prev, tag]
-        );
+        setSelected((prev) => {
+            if(prev.includes(tag)) return prev.filter((t) => t !==tag);
+            if(prev.length >= 3) return prev;
+            return [...prev, tag];
+        });
     };
 
+    const renderTags = (tags: string[]) =>
+        tags.map((label, idx) => {
+            const isSelected = selected.includes(label);
+            return(
+                <div
+                    key={idx}
+                    onClick={() => toggleTag(label)}
+                    className={`cursor-pointer flex justify-center items-center text-sm rounded-md px-7 py-2 border transition-all duration-200
+                        ${
+                            isSelected
+                                ? "text-white border-[rgba(255,255,255,0.5)]"
+                                : "border-transparent"
+                        }`}
+                    style={{
+                        backgroundColor: isSelected
+                            ? "rgba(66, 60, 53, 0.5)"
+                            : "rgba(31,28,25,0.5)"
+                    }}
+                >
+                    {label}
+                </div>
+            );
+        });
+
     return(
-        <div 
-            className="relative w-224 h-304 rounded-3xl"
-            style={{backgroundColor: 'rgba(45, 40 ,34, 1'}}>
+        <div className="fixed inset-0 z-50 flex justify-center items-center bg-opacity-50">
+            <div 
+                className="relative w-224 h-304 rounded-3xl"
+                style={{backgroundColor: 'rgba(45, 40 ,34, 1'}}>
             
-            <div className="absolute top-15 left-14">
-                <div className="text-white text-lg">
-                    목적
+                <div className="absolute top-15 left-14">
+                    <div className="text-white text-lg">
+                        목적
+                    </div>
+
+                    <div className="flex flex-row justify-center items-center text-white gap-5 mt-3">
+                        {renderTags(goalTags)}
+                    </div>
                 </div>
 
-                <div className="flex flex-row justify-center items-center text-white gap-5 mt-5">
-                    {goalTags.map((lable, idx) => {
-                        const isSelected = selected.includes(lable);
-
-                        return(
-                            <div
-                                key={idx}
-                                onClick={() => toggleTag(lable)}
-                                className={`cursor-pointer flex justify-center items-center text-sm rounded-md px-7 py-2 border transition-all duration-200
-                                    ${
-                                    isSelected
-                                        ? "text-white border-[rgba(255,255,255,0.5)]"
-                                        : "border-transparent"
-                                    }`}
-                                style={{
-                                    backgroundColor: isSelected
-                                        ? 'rgba(66,60,53,0.5)'
-                                        : 'rgba(31, 28, 25, 0.5)'
-                                }}>
-                                {lable}
-                            </div>
-                        )
-                    })}
+                <div className="absolute top-58 left-14">
+                    <div className="text-white text-lg">
+                        시간
+                    </div>
+                    <div className="flex flex-row justify-start items-center text-white gap-5 mt-3">
+                        {renderTags(timeTags)}
+                    </div>
                 </div>
-            </div>
-
-            <div className="absolute top-62 left-14">
-                <div className="text-white text-lg">
-                    시간
-                </div>
-                <div className="flex flex-row justify-start items-center text-white gap-5 mt-5">
-                    {timeTags.map((lable, idx) => {
-                        const isSelected = selected.includes(lable);
-
-                        return(
-                            <div
-                                key={idx}
-                                onClick={() => toggleTag(lable)}
-                                className={`cursor-pointer flex justify-center items-center text-sm rounded-md px-7 py-2 border transition-all duration-200
-                                    ${
-                                    isSelected
-                                        ? "text-white border-[rgba(255,255,255,0.5)]"
-                                        : "border-transparent"
-                                    }`}
-                                style={{
-                                    backgroundColor: isSelected
-                                        ? 'rgba(66,60,53,0.5)'
-                                        : 'rgba(31, 28, 25, 0.5)'
-                                }}>
-                                {lable}
-                            </div>
-                        )
-                    })}
-                </div>
-            </div>
             
-            <div className="absolute top-104 left-14 pt-5">
-                <div className="text-white text-lg">
-                    나이
+                <div className="absolute top-104 left-14">
+                    <div className="text-white text-lg">
+                        나이
+                    </div>
+                    
+                    <div className="flex flex-row justify-start items-center text-white gap-5 mt-3">
+                        {renderTags(ageTags)}
+                    </div>
+                
+                    <div className="flex flex-row justify-start items-center text-white mt-2 gap-5 ">
+                        {renderTags(particularTags)}
+                    </div>
                 </div>
-                <div className="flex flex-row justify-start items-center text-white gap-5 mt-5">
-                    {ageTags.map((lable, idx) => {
-                        const isSelected = selected.includes(lable);
 
-                        return(
-                            <div
-                                key={idx}
-                                onClick={() => toggleTag(lable)}
-                                className={`cursor-pointer flex justify-center items-center text-sm rounded-md px-7 py-2 border transition-all duration-200
-                                    ${
-                                    isSelected
-                                        ? "text-white border-[rgba(255,255,255,0.5)]"
-                                        : "border-transparent"
-                                    }`}
-                                style={{
-                                    backgroundColor: isSelected
-                                        ? 'rgba(66,60,53,0.5)'
-                                        : 'rgba(31, 28, 25, 0.5)'
-                                }}>
-                                {lable}
-                            </div>
-                        )
-                    })}
+                <div className="absolute top-164 left-14">
+                    <div className="text-white text-lg">
+                        분야
+                    </div>
+                
+                    <div className="flex flex-row justify-start items-center text-white gap-5 mt-5">
+                        {renderTags(fieldTags1)}
+                    </div>
+                    <div className="flex flex-row justify-start items-center text-white gap-5 mt-2">
+                        {renderTags(fieldTags2)}
+                    </div>
+                    <div className="flex flex-row justify-start items-center text-white gap-5 mt-2">
+                        {renderTags(fieldTags3)}
+                    </div>
+                    <div className="flex flex-row justify-start items-center text-white gap-5 mt-2">
+                        {renderTags(fieldTags4)}
+                    </div>
                 </div>
-                <div className="flex flex-row justify-start items-center text-white mt-4 gap-5 ">
-                    {particularTags.map((lable, idx) => {
-                        const isSelected = selected.includes(lable);
-
-                        return(
-                            <div
-                                key={idx}
-                                onClick={() => toggleTag(lable)}
-                                className={`cursor-pointer flex justify-center items-center text-sm rounded-md px-7 py-2 border transition-all duration-200
-                                    ${
-                                    isSelected
-                                        ? "text-white border-[rgba(255,255,255,0.5)]"
-                                        : "border-transparent"
-                                    }`}
-                                style={{
-                                    backgroundColor: isSelected
-                                        ? 'rgba(66,60,53,0.5)'
-                                        : 'rgba(31, 28, 25, 0.5)'
-                                }}>
-                                {lable}
-                            </div>
-                        )
-                    })}
-                </div>
-            </div>
-
-            <div className="absolute top-164 left-14 pt-8">
-                <div className="text-white text-lg">
-                    분야
-                </div>
-                <div className="flex flex-row justify-start items-center text-white gap-5 mt-5">
-                    {fieldTags1.map((lable, idx) => {
-                        const isSelected = selected.includes(lable);
-
-                        return(
-                            <div
-                                key={idx}
-                                onClick={() => toggleTag(lable)}
-                                className={`cursor-pointer flex justify-center items-center text-sm rounded-md px-7 py-2 border transition-all duration-200
-                                    ${
-                                    isSelected
-                                        ? "text-white border-[rgba(255,255,255,0.5)]"
-                                        : "border-transparent"
-                                    }`}
-                                style={{
-                                    backgroundColor: isSelected
-                                        ? 'rgba(66,60,53,0.5)'
-                                        : 'rgba(31, 28, 25, 0.5)'
-                                }}>
-                                {lable}
-                            </div>
-                        )
-                    })}
-                </div>
-                <div className="flex flex-row justify-start items-center text-white gap-5 mt-4">
-                    {fieldTags2.map((lable, idx) => {
-                        const isSelected = selected.includes(lable);
-
-                        return(
-                            <div
-                                key={idx}
-                                onClick={() => toggleTag(lable)}
-                                className={`cursor-pointer flex justify-center items-center text-sm rounded-md px-7 py-2 border transition-all duration-200
-                                    ${
-                                    isSelected
-                                        ? "text-white border-[rgba(255,255,255,0.5)]"
-                                        : "border-transparent"
-                                    }`}
-                                style={{
-                                    backgroundColor: isSelected
-                                        ? 'rgba(66,60,53,0.5)'
-                                        : 'rgba(31, 28, 25, 0.5)'
-                                }}>
-                                {lable}
-                            </div>
-                        )
-                    })}
-                </div>
-                <div className="flex flex-row justify-start items-center text-white gap-5 mt-4">
-                    {fieldTags3.map((lable, idx) => {
-                        const isSelected = selected.includes(lable);
-
-                        return(
-                            <div
-                                key={idx}
-                                onClick={() => toggleTag(lable)}
-                                className={`cursor-pointer flex justify-center items-center text-sm rounded-md px-7 py-2 border transition-all duration-200
-                                    ${
-                                    isSelected
-                                        ? "text-white border-[rgba(255,255,255,0.5)]"
-                                        : "border-transparent"
-                                    }`}
-                                style={{
-                                    backgroundColor: isSelected
-                                        ? 'rgba(66,60,53,0.5)'
-                                        : 'rgba(31, 28, 25, 0.5)'
-                                }}>
-                                {lable}
-                            </div>
-                        )
-                    })}
-                </div>
-                <div className="flex flex-row justify-start items-center text-white gap-5 mt-4">
-                    {fieldTags4.map((lable, idx) => {
-                        const isSelected = selected.includes(lable);
-
-                        return(
-                            <div
-                                key={idx}
-                                onClick={() => toggleTag(lable)}
-                                className={`cursor-pointer flex justify-center items-center text-sm rounded-md px-7 py-2 border transition-all duration-200
-                                    ${
-                                    isSelected
-                                        ? "text-white border-[rgba(255,255,255,0.5)]"
-                                        : "border-transparent"
-                                    }`}
-                                style={{
-                                    backgroundColor: isSelected
-                                        ? 'rgba(66,60,53,0.5)'
-                                        : 'rgba(31, 28, 25, 0.5)'
-                                }}>
-                                {lable}
-                            </div>
-                        )
-                    })}
-                </div>
-            </div>
             
-            <div className="absolute top-275 left-136 pt-4">
-                <div className="flex flex-row justify-end items-center">
-                    <button
-                        className="w-34 h-15 px-5 py-1 rounded-lg mr-7 font-bold mr-7 text-white"
-                        style={{backgroundColor: 'rgba(66,60,53,1)'}}>
-                            저장
-                    </button>
-                    <button
-                        className="w-34 h-15 px-5 py-1 rounded-lg border text-[rgba(255,255,255,0.5)] font-bold"
-                        style={{borderColor: 'rgba(255, 255, 255, 0.5)'}}>
-                            취소
-                    </button>
+                <div className="absolute top-275 left-136 pt-4">
+                    <div className="flex flex-row justify-end items-center">
+                        <button
+                            className="w-34 h-15 px-5 py-1 rounded-lg mr-7 font-bold mr-7 text-white"
+                            style={{backgroundColor: 'rgba(66,60,53,1)'}}
+                            onClick={onClose}>
+                                저장
+                        </button>
+                        <button
+                            className="w-34 h-15 px-5 py-1 rounded-lg border text-[rgba(255,255,255,0.5)] font-bold"
+                            style={{borderColor: 'rgba(255, 255, 255, 0.5)'}}
+                            onClick={onClose}>
+                                취소
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
--- src/views/reading-room/page/index.tsx
+++ src/views/reading-room/page/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import MyReadingRoomCardList from '../components/views/MyReadingRoomCardList';
+import CreateReadingRoom from '../components/views/CreateReadingRoom';
 
 const ReadingRoom = () => {
   return (
     <div className='flex flex-col justify-center items-center'>
       <div className='flex items-center justify-center'>
-        <MyReadingRoomCardList/> 
+        {/* <MyReadingRoomCardList/>  */}
+        <CreateReadingRoom/>
       </div>
     </div>
   );
