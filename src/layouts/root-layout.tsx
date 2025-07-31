e9d1cd90179b011b90d88095ac8f05b5fb6da4dc
woojo230 <woojunghyck@naver.com>
2025-07-31 12:42:36 +0900

    🎨 Design: 헤더바 숨김 처리 구현

--- src/layouts/root-layout.tsx
+++ src/layouts/root-layout.tsx
@@ -1,13 +1,19 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from '../components/header/Header';
+import { shouldHideHeader } from '../utils/layout';
 
 const RootLayout = () => {
+  const { pathname } = useLocation();
+  const showHeader = !shouldHideHeader(pathname);
+
   return (
     <div>
       <div className="min-h-screen bg-[linear-gradient(to_bottom,rgba(0,0,0,1),rgba(35,23,9,1))]">
-        <div className="flex justify-center">
-          <Header isLogin={true} />
-        </div>
+        {showHeader && (
+          <div className="flex justify-center">
+            <Header isLogin={true} />
+          </div>
+        )}
         <Outlet />
       </div>
     </div>
--- src/views/reading-room/page/private-reading-room/index.tsx
+++ src/views/reading-room/page/private-reading-room/index.tsx
@@ -1,5 +1,5 @@
 const PrivateReadingRoom = () => {
-  return <div></div>;
+  return <div className="w-[97rem] h-screen bg-red-200 m-auto"></div>;
 };
 
 export default PrivateReadingRoom;
