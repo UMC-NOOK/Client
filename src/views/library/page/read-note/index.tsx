dd5e91acb68d0b8aebfde7659b0b1cba1b3a0644
Eunjin Oh <ibsuny3214@naver.com>
2025-08-05 19:36:38 +0900

    ✨ Feat: 독서카드 저장 버튼 제작 #76

--- src/views/library/page/read-note/index.tsx
+++ src/views/library/page/read-note/index.tsx
@@ -4,6 +4,7 @@ import edit_btn from '/src/assets/button/read-note-edit/edit-btn.svg';
 import delete_btn from '/src/assets/button/read-note-edit/delete-btn.svg';
 import info_edit_btn from '/src/assets/button/read-note/info-edit-btn.svg';
 import read_note_save from '/src/assets/button/read-note/read-note-save.svg';
+import read_note_save_btn from '/src/assets/button/read-note/read-note-save-btn.svg';
 import quotation_arrow from '/src/assets/button/read-note-edit/quotation-arrow.svg';
 import impression_icon from '/src/assets/button/read-note-edit/impression-icon.svg';
 
@@ -66,8 +67,19 @@ const ReadNotePage = () => {
           </div>
         )}
       </div>
-      <div className="flex items-center justify-center mt-35">
+      <div className="flex items-center justify-center mt-35 relative ">
         <img src={read_note_save} alt="Save" className="w-[290px] h-[516px]" />
+        <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center gap-2 cursor-pointer">
+          <div className="text-[#222020] text-center text-xs not-italic font-normal">
+            독서 카드 만들고
+          </div>
+          <div className="flex items-center justify-center gap-2 px-6 py-4 rounded-md bg-[#2c251d]">
+            <img src={read_note_save_btn} alt="" className="w-9 h-9" />
+            <div className="text-white text-xs not-italic font-semibold">
+              이미지로 저장하기
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
