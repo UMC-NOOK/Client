0e8f20ea5d3b460a3321fec0a829baf74c29d0ba
Eunjin Oh <ibsuny3214@naver.com>
2025-08-08 02:31:12 +0900

    🔧 Settings: Tanstack 개발자 도구 세팅 (추후 삭제) #83

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { RouterProvider } from 'react-router-dom';
 import router from './routes/router';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 function App() {
   return (
     <>
       <RouterProvider router={router} />
+      <ReactQueryDevtools />
     </>
   );
 }
