598f3a3af96911911f86cc18f7b2132af163dc97
woojo230 <woojunghyck@naver.com>
2025-08-12 21:20:52 +0900

    빌드 오류 해결

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -43,6 +43,7 @@ const router = createBrowserRouter([
           {
             path: 'settings',
             element: <SettingsPage />,
+          },
           {
             path: 'lounge',
             errorElement: <NotFoundPage />,
