9900735c8ee3f630b9318fdcbdfb0c3858926601
Eunjin Oh <ibsuny3214@naver.com>
2025-08-18 04:09:30 +0900

    ✨ Feat: 저장된 독서기록 띄우기 #141

--- src/views/library/page/read-note/index.tsx
+++ src/views/library/page/read-note/index.tsx
@@ -1,6 +1,6 @@
 // librarys
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 // imgs
 import chevron_left from '/src/assets/button/read-note-edit/chevron-left.svg';
@@ -20,12 +20,15 @@ import DeleteBtn from '../../../../components/delete-modal/DeleteModal';
 import useGetSentenceList from '../../hooks/useMutation/read-note/useGetSentenceList';
 
 const ReadNotePage = () => {
-  const [isReadNoteExist, setIsReadNoteExist] = useState(true);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const { data: sentenceList } = useGetSentenceList(location.state.bookId);
+
+  const isReadNoteExist = (sentenceList?.result?.length ?? 0) > 0;
   const [isLibraryRegistrationOpen, setIsLibraryRegistrationOpen] =
     useState(false);
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
-
-  const { data: sentenceList } = useGetSentenceList('bookId123'); // 예시 bookId, 실제로는 props나 context에서 받아와야 함
 
   const handleLibrary = () => {
     // 수정 로직 추가
@@ -45,15 +48,16 @@ const ReadNotePage = () => {
     setIsDeleteModalOpen(false);
   };
 
-  const navigate = useNavigate();
-  const id = '3333'; // 예시 ID, 실제로는 props나 context에서 받아와야 함
-
   return (
     <div className="flex items-start justify-center w-full h-full gap-23 mt-20">
       {isLibraryRegistrationOpen && (
         <LibraryRegistration
           onRegister={handleLibrary}
           closeModal={libraryModalHandler}
+          bookImg={location.state?.bookImg}
+          bookTitle={location.state?.title}
+          bookAuthor={location.state?.author}
+          bookId={location.state?.bookId}
         />
       )}
       {isDeleteModalOpen && (
@@ -66,13 +70,18 @@ const ReadNotePage = () => {
       <div className="flex flex-col items-center justify-start w-332">
         <div className="flex w-full h-35 items-center justify-between backdrop-blur-[20px] border-b border-solid border-b-[rgba(85,83,81,1)]">
           <div className="flex items-center gap-20">
-            <img src={chevron_left} alt="chevron left" className="h-10 w-10" />
+            <img
+              src={chevron_left}
+              alt="chevron left"
+              className="h-10 w-10"
+              onClick={() => navigate(-1)}
+            />
             <div className="flex items-end gap-7">
               <span className="text-white text-[22px] not-italic font-semibold leading-[25px]">
-                칵테일, 러브, 좀비
+                {location.state?.title || '책 제목 데이터 없음'}
               </span>
               <p className="text-white text-xs not-italic font-normal">
-                조예은
+                {location.state?.author || '저자 데이터 없음'}
               </p>
             </div>
           </div>
@@ -81,7 +90,7 @@ const ReadNotePage = () => {
               src={edit_btn}
               alt="Edit"
               className="w-17 h-17 cursor-pointer"
-              onClick={() => navigate(`/library/${id}/edit`)}
+              onClick={() => navigate(`/library/${location.state.bookId}/edit`)}
             />
             <img
               src={info_edit_btn}
@@ -99,33 +108,22 @@ const ReadNotePage = () => {
         </div>
         {isReadNoteExist ? (
           <div className="flex flex-col items-start justify-start w-[634px] h-[478px] gap-7 mt-20 overflow-y-auto  [&::-webkit-scrollbar]:hidden">
-            <Phrase
-              text="너무 사소해서 남에게 말하기조차 민망하지만 확실히 나의 신경을 자극하는 것. 존재하지 않지만 나에게는 느껴지는 것. 그런 걸 어떻게 다뤄야 하는지 나는 알지 못했다."
-              page={1}
-            />
-            <Impression text="보이지 않지만 분명히 느껴는 감정의 결을 섬세하게 포착한 문장이다. 이해받기 어려운 내면의 진동을 조용히 꺼내 보여주는 듯해 마음에 오래 머문다." />
-            <Phrase
-              text="시간이 많아지면 생각이 많아지고, 생각이 많아지면 우울이 찾아들기 마련이다."
-              page={45}
-            />
-            <Phrase
-              text="너무 사소해서 남에게 말하기조차 민망하지만 확실히 나의 신경을 자극하는 것. 존재하지 않지만 나에게는 느껴지는 것. 그런 걸 어떻게 다뤄야 하는지 나는 알지 못했다. 너무 사소해서 남에게 말하기조차 민망하지만 확실히 나의 신경을 자극하는 것. 존재하지 않지만 나에게는 느껴지는 것. 그런 걸 어떻게 다뤄야 하는지 나는 알지 못했다."
-              page={112}
-            />
-            <Phrase
-              text="너무 사소해서 남에게 말하기조차 민망하지만 확실히 나의 신경을 자극하는 것. 존재하지 않지만 나에게는 느껴지는 것. 그런 걸 어떻게 다뤄야 하는지 나는 알지 못했다. 너무 사소해서 남에게 말하기조차 민망하지만 확실히 나의 신경을 자극하는 것. 존재하지 않지만 나에게는 느껴지는 것. 그런 걸 어떻게 다뤄야 하는지 나는 알지 못했다."
-              page={112}
-            />
-            <Phrase
-              text="너무 사소해서 남에게 말하기조차 민망하지만 확실히 나의 신경을 자극하는 것. 존재하지 않지만 나에게는 느껴지는 것. 그런 걸 어떻게 다뤄야 하는지 나는 알지 못했다. 너무 사소해서 남에게 말하기조차 민망하지만 확실히 나의 신경을 자극하는 것. 존재하지 않지만 나에게는 느껴지는 것. 그런 걸 어떻게 다뤄야 하는지 나는 알지 못했다."
-              page={112}
-            />
-            <Quotation
-              text={[
-                '조예은 작가의 『칵테일, 러브, 좀비』는 좀비 아포칼립스 속에서도 사랑과 일상이 존재할 수 있다는 사실을 낯설지만 설득력 있게 보여준다. 현실적인 감정과 비현실적인 상황이 자연스럽게 섞이면서, 오히려 더 진짜 같은 이야기로 다가왔다. 주인공들의 관계는 감정에 휩쓸리기보다 조심스럽게 서로를 알아가는 과정이라 더 인상 깊었다. 좀비보다 더 위협적인 것은 사람들 사이의 오해나 무관심이라는 점도 묵직하게 남는다. 장르적 재미와 감정의 진심이 잘 어우러진, 독특하고도 따뜻한 작품이었다.',
-                '좀비로 변한 아버지를 향해 총을 든 엄마의 모습은 충격적이었지만, 동시에 오랜 억압을 스스로 끊어내는 해방의 순간으로 느껴졌다. 침묵 속에 있던 엄마가 주체적으로 행동하는 모습은 가부장적 구조에 균열을 내는 장면처럼 다가왔고, 그 변화가 줄 수 있는 희망을 떠올리게 했다.',
-              ]}
-            />
+            {sentenceList?.result.map((sentence, index) => (
+              <>
+                {sentence.recordType === 'RECORD' ? (
+                  <Phrase
+                    key={sentence.recordId}
+                    text={sentence.content}
+                    page={parseInt(sentence.page, 10)}
+                  />
+                ) : (
+                  <Impression key={sentence.recordId} text={sentence.content} />
+                )}
+                {sentence.comments.map((comment) => (
+                  <Quotation key={comment.commentId} text={comment.content} />
+                ))}
+              </>
+            ))}
           </div>
         ) : (
           <div className="text-[rgba(255,255,255,0.50)] text-center text-base not-italic font-normal flex items-center justify-center w-[634px] h-[478px]">
@@ -162,7 +160,7 @@ const Phrase = ({ text, page }: { text: string; page: number }) => {
   );
 };
 
-const Impression = ({ text }: { text: string }) => {
+const Quotation = ({ text }: { text: string }) => {
   return (
     <div className="w-full flex items-start justify-start gap-10 text-white text-sm not-italic font-normal pl-30">
       <img src={quotation_arrow} alt="" className="w-10 h-10" />
@@ -171,21 +169,15 @@ const Impression = ({ text }: { text: string }) => {
   );
 };
 
-const Quotation = ({ text }: { text: string[] }) => {
+const Impression = ({ text }: { text: string }) => {
   return (
-    <div className="w-full flex items-start justify-start gap-10 text-white text-sm not-italic font-normal">
+    <div className="w-full flex items-center justify-start gap-10 text-white text-sm not-italic font-normal">
       <img
         src={impression_icon}
         alt="impression"
         className="w-[33px] h-[39px]"
       />
-      <div className="w-full flex flex-col gap-7">
-        {text.map((line, index) => (
-          <p key={index} className="mb-2">
-            {line}
-          </p>
-        ))}
-      </div>
+      <div className="w-full flex flex-col gap-7">{text}</div>
     </div>
   );
 };
--- src/views/library/types/read-note/sentenceList.ts
+++ src/views/library/types/read-note/sentenceList.ts
@@ -8,7 +8,7 @@ export interface Sentence {
   recordId: number;
   page: string;
   content: string;
-  recordType: 'RECORD' | 'SENTENCE';
+  recordType: 'RECORD' | 'COMMENTARY'; // 'RECORD' 문장, 'COMMENTARY' 감상
   createdDate: string;
   comments: Comment[];
 }
