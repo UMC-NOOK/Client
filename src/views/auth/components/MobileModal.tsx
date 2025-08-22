73c85e04642ff1dbef2496d0c1119d2e61a63bba
woojo230 <woojunghyck@naver.com>
2025-08-22 14:49:47 +0900

    ♻️ Refactor: 필요없는 주석 삭제

--- src/views/auth/components/MobileModal.tsx
+++ src/views/auth/components/MobileModal.tsx
@@ -1,15 +1,11 @@
-// src/components/MobileModal.tsx
-
 import forPCBtn from '../../../assets/auth/plzPC.svg';
 
-// ✨ 모달을 닫는 함수를 props로 받도록 수정
 interface MobileModalProps {
   onClose: () => void;
 }
 
 const MobileModal = ({ onClose }: MobileModalProps) => {
   return (
-    // 1. 전체 화면을 덮는 반투명 배경 (모달 오버레이)
     <div className="fixed inset-0 bg-opacity-50 flex items-center justify-center z-50 backdrop-blur-md">
       <div className="relative">
         <img src={forPCBtn} className="object-contain" alt="PC 환경 권장" />
