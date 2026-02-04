205cd4f40ff789795557c86c7b90a1e739c8e9a2
seoyeon0103 <angel6462@naver.com>
2026-02-04 23:42:24 +0900

    Feat: IconComponent 추가

---
+++ src/components/action/IconComponent.tsx
@@ -0,0 +1,30 @@
+import type { HTMLAttributes, ReactNode } from "react";
+
+type IconSize = "xs" | "s" | "m";
+
+type IconFrameProps = {
+  children: ReactNode; //화면에 렌더링할 수 있는 모든 것(문자 등등등...)
+  size?: IconSize;
+  className?: string;
+} & HTMLAttributes<HTMLSpanElement>;
+
+const base = "items-center justify-center";
+
+const sizeClassMap: Record<IconSize, string> = {
+  xs: "inline-flex w-[18px] h-[18px] p-0.5",
+  s: "inline-flex w-6 h-6 p-0.5",
+  m: "inline-flex w-10 h-10 p-2",
+};
+
+export default function IconComponent({
+  children,
+  size = "s",
+  className = "",
+  ...props
+}: IconFrameProps) {
+  return (
+    <span className={[sizeClassMap[size], base, className].join(" ")} {...props}>
+      {children}
+    </span>
+  );
+}
--- src/components/action/TestButtonComponent.tsx
+++ src/components/action/TestButtonComponent.tsx
@@ -1,5 +1,6 @@
 import Button from "./ButtonComponent";
 import Chip from "./ChipComponent";
+import IconComponent from "./IconComponent";
 
 export default function TestButtonComponet() {
   return (
@@ -40,6 +41,27 @@ export default function TestButtonComponet() {
           <Chip variant="selected" onClick={() => alert("selected chip!")}>
             선택 칩 클릭
           </Chip>
+        </div>
+      </section>
+
+      <section className="space-y-3">
+        <h2 className="text-base font-semibold">IconFrame (size="s")</h2>
+
+        <div className="flex items-center gap-4">
+            <IconComponent size="s">
+                <svg
+                    width="18"
+                    height="18"
+                    viewBox="0 0 18 18"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-full h-full">
+                    <path
+                        d="M16.2196 0.219608C16.5124 -0.0732298 16.9872 -0.0731753 17.2802 0.219608C17.573 0.512442 17.5729 0.987234 17.2802 1.28015L9.81336 8.74988L17.2802 16.2196C17.5729 16.5125 17.573 16.9873 17.2802 17.2802C16.9872 17.5729 16.5124 17.573 16.2196 17.2802L8.75281 9.81043L1.28015 17.2802C0.987234 17.5729 0.512442 17.573 0.219608 17.2802C-0.0731754 16.9872 -0.0732298 16.5124 0.219608 16.2196L7.69226 8.74988L0.219608 1.28015C-0.0732298 0.98732 -0.0731754 0.512528 0.219608 0.219608C0.512442 -0.0732298 0.987234 -0.0731754 1.28015 0.219608L8.75281 7.68933L16.2196 0.219608Z"
+                        fill="#ECECEC"
+                    />
+                </svg>
+            </IconComponent>
         </div>
       </section>
     </div>
