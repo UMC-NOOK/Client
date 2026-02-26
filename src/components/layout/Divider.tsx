476b92fc4f3169a70cdf24f164a1193f8cf19c14
Eunjin Oh <ibsuny3214@naver.com>
2026-02-26 15:59:19 +0900

    ✨ Feat: layout-divider 컴포넌트 수정 #235

--- src/components/layout/Divider.tsx
+++ src/components/layout/Divider.tsx
@@ -1,4 +1,9 @@
-export default function Divider(width: number, height: number) {
+type DividerProps = {
+  width: number;
+  height: number;
+};
+
+export default function Divider({ width, height }: DividerProps) {
   return (
     <div
       className={`bg-gradient-divider flex w-${width} h-${height} justify-center items-center`}
