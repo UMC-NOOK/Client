7d4b9f4c7e51bc82c04a2da288602b82e050db44
Eunjin Oh <ibsuny3214@naver.com>
2026-02-26 16:09:15 +0900

    ✨ Feat: layout-dim 컴포넌트 수정 #235

--- src/components/layout/Dim.tsx
+++ src/components/layout/Dim.tsx
@@ -1,8 +1,10 @@
 type DimProps = {
-  width: number;
-  height: number;
+  width: number | string;
+  height: number | string;
 };
 
 export default function Dim({ width, height }: DimProps) {
-  return <div className={`w-${width} h-${height} bg-black/50`} />;
+  return <div className={`w-${width} h-${height} bg-black/50 absolute`} />;
 }
+
+// 해당 컴포넌트를 위에 쌓으려면 부모 요소에 relative를 주어야 함
