d1742af93f281df669b863f8bd5723066e76f85d
Eunjin Oh <ibsuny3214@naver.com>
2026-03-05 03:40:00 +0900

    ✨ Feat: Atomic-Theme 컴포넌트 개발 #251

---
+++ src/components/atomic/Theme.tsx
@@ -0,0 +1,22 @@
+type ThemeProps = {
+  imageUrl: string;
+  select: boolean;
+  onClick: () => void;
+};
+
+export default function Theme({ imageUrl, select, onClick }: ThemeProps) {
+  return (
+    <div className="w-20 h-20 " onClick={onClick}>
+      <img
+        src={imageUrl}
+        alt="Theme"
+        className={
+          `rounded-md  w-full h-full object-cover` +
+          (select
+            ? "outline-1 outline-gray-90 outline-solid"
+            : "w-full h-full opacity-50")
+        }
+      />
+    </div>
+  );
+}
