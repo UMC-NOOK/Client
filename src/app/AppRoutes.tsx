af56eb404301eda11cea90394ecd86004b1aeea4
seoyeon0103 <angel6462@naver.com>
2026-03-09 17:25:59 +0900

    feat: text button component  만들기

--- src/app/AppRoutes.tsx
+++ src/app/AppRoutes.tsx
@@ -15,6 +15,7 @@ import SearchNewAddMorePage from "../pages/search/SearchNewAddMorePage";
 import BannerActionCardTestPage from "../pages/search/test/testpage";
 import BottomSheetTestPage from "../pages/search/test/BottomSheetTestPage";
 import PopupConfirmModalTestPage from "../pages/search/test/PopupTestPage";
+import TextButtonTestPage from "../pages/search/TextTestPage";
 
 
 type TabKey = "library" | "focus" | "record" | "group";
@@ -91,8 +92,6 @@ export default function AppRoutes() {
           <Route path="/test/banner-action-card" element={<BannerActionCardTestPage />} />
           <Route path="/test/bottomsheet" element={<BottomSheetTestPage />} />
           <Route path="/test/popup" element={<PopupConfirmModalTestPage/>} />
-
-
         </Route>
 
         <Route path="*" element={<Navigate to="/library" replace />} />
