cbbd75e277be63597fcf8e73c304c8c2d53f6e7b
seoyeon0103 <angel6462@naver.com>
2026-03-09 17:31:23 +0900

    fix: build 오류 원인 고치기

--- src/components/action/Button/FAB.tsx
+++ src/components/action/Button/FAB.tsx
@@ -1,4 +1,4 @@
-import React, { type ReactNode } from "react";
+import { type ReactNode } from "react";
 
 type Props = {
   icon: ReactNode; 
@@ -20,7 +20,7 @@ export function FAB({
         "inline-flex items-center rounded-[32px] select-none p-2 bg-gray-90", //select-none : svg 가 파란색으로 선택되지 않도록 방지하는 용도
       ].join(" ")}
     >
-        <span className="flex h-6 w-6 justify-center item-center">
+        <span className="flex h-6 w-6 justify-center items-center">
                 {icon}
         </span>
     </div>
