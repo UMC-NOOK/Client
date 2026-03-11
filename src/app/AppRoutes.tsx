e48b8f697cf6c875c12ba7436720f5b809bf224f
seoyeon0103 <angel6462@naver.com>
2026-03-12 02:13:36 +0900

    fix: test page 삭제

--- src/app/AppRoutes.tsx
+++ src/app/AppRoutes.tsx
@@ -15,7 +15,6 @@ import SearchNewAddMorePage from "../pages/search/SearchNewAddMorePage";
 import BannerActionCardTestPage from "../pages/search/test/testpage";
 import BottomSheetTestPage from "../pages/search/test/BottomSheetTestPage";
 import PopupConfirmModalTestPage from "../pages/search/test/PopupTestPage";
-import ChipTestPage from "../pages/search/ChipTestPage";
 
 
 type TabKey = "library" | "focus" | "record" | "group";
@@ -92,8 +91,6 @@ export default function AppRoutes() {
           <Route path="/test/banner-action-card" element={<BannerActionCardTestPage />} />
           <Route path="/test/bottomsheet" element={<BottomSheetTestPage />} />
           <Route path="/test/popup" element={<PopupConfirmModalTestPage/>} />
-
-          <Route path="/chip" element={<ChipTestPage/>}/>
         </Route>
 
         <Route path="*" element={<Navigate to="/library" replace />} />
