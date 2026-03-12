ae1084fd31a024a6a15f7c098c4e72c870f6215f
seoyeon0103 <angel6462@naver.com>
2026-03-12 17:42:15 +0900

    refactor: testpage 들 삭제

--- src/app/AppRoutes.tsx
+++ src/app/AppRoutes.tsx
@@ -15,8 +15,6 @@ import SearchNewAddMorePage from "../pages/search/SearchNewAddMorePage";
 import BannerActionCardTestPage from "../pages/search/test/testpage";
 import BottomSheetTestPage from "../pages/search/test/BottomSheetTestPage";
 import PopupConfirmModalTestPage from "../pages/search/test/PopupTestPage";
-import SectionHeaderTestPage from "../pages/search/SectionHeaderTestPage";
-import InformationSectionTestPage from "../pages/search/InformationSectionHeaderTestPage";
 
 
 type TabKey = "library" | "focus" | "record" | "group";
@@ -93,9 +91,6 @@ export default function AppRoutes() {
           <Route path="/test/banner-action-card" element={<BannerActionCardTestPage />} />
           <Route path="/test/bottomsheet" element={<BottomSheetTestPage />} />
           <Route path="/test/popup" element={<PopupConfirmModalTestPage/>} />
-
-          <Route path="/sectionHeader" element={<SectionHeaderTestPage/>}/>
-          <Route path="/infoSection" element={<InformationSectionTestPage/>}/>
         </Route>
 
         <Route path="*" element={<Navigate to="/library" replace />} />
