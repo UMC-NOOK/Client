893d283542f991d0e3bcc721630cbfc96ed1a584
seoyeon0103 <angel6462@naver.com>
2026-03-29 21:51:07 +0900

    feat: checkbox component 만들기

--- src/app/AppRoutes.tsx
+++ src/app/AppRoutes.tsx
@@ -15,6 +15,7 @@ import SearchNewAddMorePage from "../pages/search/SearchNewAddMorePage";
 import BannerActionCardTestPage from "../pages/search/test/testpage";
 import BottomSheetTestPage from "../pages/search/test/BottomSheetTestPage";
 import PopupConfirmModalTestPage from "../pages/search/test/PopupTestPage";
+import CheckboxTestpage from "../pages/search/CheckboxTest";
 
 type TabKey = "library" | "focus" | "record" | "group";
 
@@ -90,6 +91,7 @@ export default function AppRoutes() {
           <Route path="/test/banner-action-card" element={<BannerActionCardTestPage />} />
           <Route path="/test/bottomsheet" element={<BottomSheetTestPage />} />
           <Route path="/test/popup" element={<PopupConfirmModalTestPage/>} />
+          <Route path="/test/checkbox" element={<CheckboxTestpage/>}/>
         </Route>
 
         <Route path="*" element={<Navigate to="/library" replace />} />
---
+++ src/pages/search/CheckboxTest.tsx
@@ -0,0 +1,7 @@
+import Checkbox from "../../components/section/checkbox/Checkbox";
+
+export default function CheckboxTestpage() {
+    return(
+        <Checkbox text="되냐"/>
+    );
+}
