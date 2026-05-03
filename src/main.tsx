a0599771498f95fa2a224604b863574403749874
Eunjin Oh <ibsuny3214@naver.com>
2026-05-03 22:31:15 +0900

    feat: tanstack 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,24 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "./index.css";
 import App from "./App.tsx";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1, // 실패 시 1번만 재시도 (기본값 3)
+      refetchOnWindowFocus: false, // 창을 탭했다가 돌아올 때마다 재호출 방지 (선택사항)
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>,
 );
