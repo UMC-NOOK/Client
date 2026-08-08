1fb9e8da0ef3dbbf7d950395dbe352b3c700f8ad
p1001q <qrwrer123@gmail.com>
2026-08-09 01:30:11 +0900

    feat: 포커스 도메인 서브 페이지 신규 생성 및 라우트 연결 #333

--- src/pages/focus/FocusMainPage.tsx
+++ src/pages/focus/FocusMainPage.tsx
@@ -1,8 +1,8 @@
-export default function FocusMobilePage() {
+export default function FocusMainPage() {
   return (
     <div className="mt-8 text-gray-90">
-      {/* TODO(담당자): 포커스 페이지 UI 구현 */}
-      <div className="text-[20px] font-semibold">포커스 (TODO)</div>
+      {/* TODO(담당자): 포커스 메인 UI 구현 (Issue #333) */}
+      <div className="text-[20px] font-semibold">포커스 메인 (TODO)</div>
     </div>
   );
 }
---
+++ src/pages/focus/FocusThemePage.tsx
@@ -0,0 +1,8 @@
+export default function FocusThemePage() {
+  return (
+    <div className="mt-8 text-gray-90">
+      {/* TODO(담당자): 포커스 테마 선택 UI 구현 */}
+      <div className="text-[20px] font-semibold">포커스 테마 선택 (TODO)</div>
+    </div>
+  );
+}
