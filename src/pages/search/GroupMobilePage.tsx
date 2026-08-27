8dba66635e79a160b022f8b1b8a37b349bba2089
p1001q <qrwrer123@gmail.com>
2026-08-27 13:44:57 +0900

    feat: 그룹 Coming Soon 화면 구현 #343

--- src/pages/search/GroupMobilePage.tsx
+++
@@ -1,8 +0,0 @@
-export default function GroupMobilePage() {
-  return (
-    <div className="mt-8 text-gray-90">
-      {/* TODO(담당자): 그룹 페이지 UI 구현 */}
-      <div className="text-[20px] font-semibold">그룹 (TODO)</div>
-    </div>
-  );
-}
